034c8d5a0eef6e4369d49fc839dc09c6ec45f2dd
ruizadrian <aruiz25@lion.lmu.edu>
2016-04-03 05:12:05 -0700

    Update load.js

--- Unicat Escape /src/load.js
+++ Unicat Escape /src/load.js
@@ -9,28 +9,15 @@ var loadState = {
         progressBar.anchor.setTo(0.5, 0.5);
         game.load.setPreloadSprite(progressBar);
 
-        game.load.image('background', 'assets/background/background.png');
-
-        game.load.audio('power', 'audio/powerup.mp3');
-        game.load.audio('soundtrack_menu', 'audio/technicko.mp3');
-        game.load.audio('thud', 'audio/thud.mp3');
-        game.load.audio('moving', 'audio/move.mp3');
-        game.load.audio('death', 'audio/puff.mp3');
-
-        game.load.image('powerup', 'assets/background/powerup.png');
-        game.load.image('life_particle', 'assets/background/particle_life.png');
-
         game.load.image('empty_heart', 'assets/ui/empty_heart.png');
         game.load.image('full_heart', 'assets/ui/full_heart.png');
         game.load.image('play_button', 'assets/ui/play_button.png');
         game.load.image('help_button', 'assets/ui/help_button.png');
         game.load.image('credits_button', 'assets/ui/info_button.png');
-        game.load.image('help_image', 'assets/ui/how_to_play.png');
         game.load.spritesheet('mute_button', 'assets/ui/mute_button.png', 154, 200);
 
         game.load.image('menu_background', 'assets/ui/menu_background.png');
         game.load.image('title', 'assets/ui/title.png');
-        game.load.image('defeat', 'assets/ui/defeat.png');
         game.load.image('back_button', 'assets/ui/back_icon.png');
 
     },
